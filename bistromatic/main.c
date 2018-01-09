f5f2dc13744df498faa80d157c9134d42bbf77b6
Hackathon <hackathon@e1z4r9p18.42.us.org>
2018-01-08 17:15:02 -0800

    started the main, worked on i/o

--- bistromatic/main.c
+++ bistromatic/main.c
@@ -2,5 +2,16 @@
 
 int	main(int argc, char **argv)
 {
+	t_bistro	*bistro;
+	int			result;
+
+	bistro = ft_memalloc(sizeof(t_bistro));
+	if (!bistro || argc != 3)
+		return (0);
+	read_base(argv[1], bistro);
+	read_stdin(ft_atoi(argv[2]), bistro);
+	//create_btree(bistro);
+	//result = eval_btree(bistro);
+	digitizer(result, bistro);
 	return (0);
 }
